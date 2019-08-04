87ee9109c436d69d96ac66a1938ca161aa6b62d8
SidGoyal2014 <50585327+SidGoyal2014@users.noreply.github.com>
2019-08-04 22:09:01 +0530

    Create numberpattern1.cpp

---
+++ numberpattern1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){																		//11111
+	int i,j,n;																	//11111
+	cin>>n;																		  //11111
+	for(i=1;i<=n;i++){													//11111
+		for(j=1;j<=n;j++){												//11111
+			cout<<"1";
+		}						      												// 'i' is for row
+		cout<<endl;																// 'j' is for column
+	}
+	return 0;
+}
