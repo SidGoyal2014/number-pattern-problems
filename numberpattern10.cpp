9c9c8ae5bae7590deabacf44aeee7f3a2af0a86b
SidGoyal2014 <50585327+SidGoyal2014@users.noreply.github.com>
2019-08-04 22:20:20 +0530

    Create numberpattern10.cpp

---
+++ numberpattern10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main(){
+	int i,j,n;
+	cout<<"enter number of lines: ";				//1
+	cin>>n;											            //22
+	cout<<"The pattern is: \n";						  //333
+	for(i=1;i<=n;i++){								      //4444
+		for(j=1;j<=i;j++){							      //55555
+			cout<<i;								            // number pattern problem
+		}
+		cout<<endl;
+	}
+	return 0;
+}
