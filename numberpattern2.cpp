f6c4a67a310065edc17a2c01657c6d9d7fa4c10b
SidGoyal2014 <50585327+SidGoyal2014@users.noreply.github.com>
2019-08-04 22:09:50 +0530

    Create numberpattern2.cpp

---
+++ numberpattern2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){																	//11111
+	int n,i,j;																//00000
+	cin>>n;																	  //11111
+	for(i=1;i<=n;i++){								  			//00000
+		if(i%2==0){															//11111
+			for(j=1;j<=n;j++){										// 'i' is of row
+				cout<<"0";													// 'j' is of column
+			}
+		}
+		else{
+			for(j=1;j<=n;j++){
+				cout<<"1";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
