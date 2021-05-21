0186287992849d65773d6b067f8f89504d922482
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-05-21 23:18:53 +0530

    Update numberpattern2.cpp

--- numberpattern2.cpp
+++ numberpattern2.cpp
@@ -1,12 +1,23 @@
+/*
+n: 5
+11111
+00000
+11111
+00000
+11111
+*/
+
+////////// CODE ////////////
+
 #include <iostream>
 using namespace std;
-int main(){										//  11111
-	int n,i,j;									//  00000
-	cin>>n;										//  11111
-	for(i=1;i<=n;i++){								//  00000
-		if(i%2==0){								//  11111
-			for(j=1;j<=n;j++){						// 'i' is of row
-				cout<<"0";						// 'j' is of column
+int main(){
+	int n,i,j;
+	cin>>n;
+	for(i=1;i<=n;i++){
+		if(i%2==0){
+			for(j=1;j<=n;j++){
+				cout<<"0";
 			}
 		}
 		else{
